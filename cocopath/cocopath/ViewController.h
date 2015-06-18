b7973352080a69884ec2ed2305c14235ca039674
strawbeery <dongcaili2012@163.com>
2015-06-18 14:34:43 +0800

    1

---
+++ cocopath/cocopath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocopath
+//
+//  Created by 董彩丽 on 15/6/15.
+//  Copyright (c) 2015年 董彩丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cocopath/cocopath/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cocopath
+//
+//  Created by 董彩丽 on 15/6/15.
+//  Copyright (c) 2015年 董彩丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
